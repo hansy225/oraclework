1f24f332693dff31a3a54bf4a0483fa4a3edd342
HanSuyeon <sy02251105@gmail.com>
2025-02-03 18:27:52 +0900

    0203

--- 02.tjoeun계정.sql
+++ 02.tjoeun계정.sql
@@ -460,4 +460,47 @@ FROM employee
 WHERE SALARY >= 2000000 AND SALARY <= 5000000 AND HIRE_DATE >= '01/01/01' AND BONUS IS NULL;
 
 --5. 보너스포함 연봉이 NULL이 아니고 이름에 '하'가 포함되어 있는 사원들의 사번, 사원명, 급여, 보너스포함연봉 조회 (별칭부여)
+-- SELECT EMP_ID, EMP_NAME, SALARY, 
 
+------------------------------- 종합 문제 ----------------------------------
+-- 1. JOB 테이블에서 모든 정보 조회
+SELECT *
+FROM JOB;
+
+-- 2. JOB 테이블에서 직급 이름 조회
+SELECT JOB_NAME
+FROM job;
+
+-- 3. DEPARTMENT 테이블에서 모든 정보 조회
+SELECT *
+FROM department;
+
+-- 4. EMPLOYEE테이블의 직원명, 이메일, 전화번호, 고용일 조회
+SELECT EMP_NAME, EMAIL, PHONE, HIRE_DATE
+FROM employee;
+
+-- 5. EMPLOYEE테이블의 고용일, 사원 이름, 월급 조회
+SELECT HIRE_DATE, EMP_NAME, SALARY
+FROM employee;
+
+-- 6. EMPLOYEE테이블에서 이름, 연봉, 총수령액(보너스포함), 실수령액(총수령액 - (연봉*세금 3%)) 조회
+SELECT EMP_NAME, SALARY*12, (SALARY*BONUS)+SALARY 총수령액
+FROM employee;
+
+-- 7. EMPLOYEE테이블에서 JOB_CODE가 J1인 사원의 이름, 월급, 고용일, 연락처 조회
+
+-- 8. EMPLOYEE테이블에서 실수령액(6번 참고)이 5천만원 이상인 사원의 이름, 월급, 실수령액, 고용일 조회
+
+-- 9. EMPLOYEE테이블에 월급이 4000000이상이고 JOB_CODE가 J2인 사원의 전체 내용 조회
+
+-- 10. EMPLOYEE테이블에 DEPT_CODE가 D9이거나 D5인 사원 중 
+--     고용일이 02년 1월 1일보다 빠른 사원의 이름, 부서코드, 고용일 조회
+
+-- 11. EMPLOYEE테이블에 고용일이 90/01/01 ~ 01/01/01인 사원의 전체 내용을 조회
+
+-- 12. EMPLOYEE테이블에서 이름 끝이 '연'으로 끝나는 사원의 이름 조회
+
+-- 13. EMPLOYEE테이블에서 전화번호 처음 3자리가 010이 아닌 사원의 이름, 전화번호를 조회
+
+-- 14. EMPLOYEE테이블에서 메일주소 '_'의 앞이 4자이면서 DEPT_CODE가 D9 또는 D6이고 
+--     고용일이 90/01/01 ~ 00/12/01이고, 급여가 270만 이상인 사원의 전체를 조회
